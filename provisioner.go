23b1b5a1fc10cae963c099ea34ed51d6e85b669e
Simon Tucker <brotherlogic@gmail.com>
2021-12-23 12:34:52 -0800

    Install

--- provisioner.go
+++ provisioner.go
@@ -605,6 +605,7 @@ func (s *Server) setAutoload() {
 
 	exec.Command("apt", "install", "-y", "chromium-browser").Run()
 	exec.Command("apt", "install", "-y", "unclutter").Run()
+	exec.Command("apt", "install", "-y", "point-rpi").Run()
 	exec.Command("rm", "/etc/xdg/autostart/piwiz.desktop").Run()
 
 	for _, string := range []string{"@lxpanel --profile LXDE-pi",
