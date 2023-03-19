dc1df996476c74dff0b96128e948fc336fe34179
Simon Tucker <brotherlogic@gmail.com>
2023-03-19 08:32:51 -0700

    Cleaning up

--- proto/provisioner.pb.go
+++ proto/provisioner.pb.go
@@ -10,7 +10,6 @@ import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
-	sync "sync"
 )
 
 const (
@@ -20,124 +19,19 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type ClusterRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-}
-
-func (x *ClusterRequest) Reset() {
-	*x = ClusterRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_provisioner_proto_msgTypes[0]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ClusterRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ClusterRequest) ProtoMessage() {}
-
-func (x *ClusterRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_provisioner_proto_msgTypes[0]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ClusterRequest.ProtoReflect.Descriptor instead.
-func (*ClusterRequest) Descriptor() ([]byte, []int) {
-	return file_provisioner_proto_rawDescGZIP(), []int{0}
-}
-
-type ClusterResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	ToAdd string `protobuf:"bytes,1,opt,name=to_add,json=toAdd,proto3" json:"to_add,omitempty"`
-}
-
-func (x *ClusterResponse) Reset() {
-	*x = ClusterResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_provisioner_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ClusterResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ClusterResponse) ProtoMessage() {}
-
-func (x *ClusterResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_provisioner_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ClusterResponse.ProtoReflect.Descriptor instead.
-func (*ClusterResponse) Descriptor() ([]byte, []int) {
-	return file_provisioner_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *ClusterResponse) GetToAdd() string {
-	if x != nil {
-		return x.ToAdd
-	}
-	return ""
-}
-
 var File_provisioner_proto protoreflect.FileDescriptor
 
 var file_provisioner_proto_rawDesc = []byte{
 	0x0a, 0x11, 0x70, 0x72, 0x6f, 0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x65, 0x72, 0x2e, 0x70, 0x72,
 	0x6f, 0x74, 0x6f, 0x12, 0x0b, 0x70, 0x72, 0x6f, 0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x65, 0x72,
-	0x22, 0x10, 0x0a, 0x0e, 0x43, 0x6c, 0x75, 0x73, 0x74, 0x65, 0x72, 0x52, 0x65, 0x71, 0x75, 0x65,
-	0x73, 0x74, 0x22, 0x28, 0x0a, 0x0f, 0x43, 0x6c, 0x75, 0x73, 0x74, 0x65, 0x72, 0x52, 0x65, 0x73,
-	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x15, 0x0a, 0x06, 0x74, 0x6f, 0x5f, 0x61, 0x64, 0x64, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x6f, 0x41, 0x64, 0x64, 0x32, 0x0f, 0x0a, 0x0d,
-	0x43, 0x6c, 0x75, 0x73, 0x65, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x42, 0x2b, 0x5a,
-	0x29, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x62, 0x72, 0x6f, 0x74,
-	0x68, 0x65, 0x72, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x2f, 0x70, 0x72, 0x6f, 0x76, 0x69, 0x73, 0x69,
-	0x6f, 0x6e, 0x65, 0x72, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x32, 0x0f, 0x0a, 0x0d, 0x43, 0x6c, 0x75, 0x73, 0x65, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x42, 0x2b, 0x5a, 0x29, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f,
+	0x62, 0x72, 0x6f, 0x74, 0x68, 0x65, 0x72, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x2f, 0x70, 0x72, 0x6f,
+	0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x65, 0x72, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var (
-	file_provisioner_proto_rawDescOnce sync.Once
-	file_provisioner_proto_rawDescData = file_provisioner_proto_rawDesc
-)
-
-func file_provisioner_proto_rawDescGZIP() []byte {
-	file_provisioner_proto_rawDescOnce.Do(func() {
-		file_provisioner_proto_rawDescData = protoimpl.X.CompressGZIP(file_provisioner_proto_rawDescData)
-	})
-	return file_provisioner_proto_rawDescData
-}
-
-var file_provisioner_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_provisioner_proto_goTypes = []interface{}{
-	(*ClusterRequest)(nil),  // 0: provisioner.ClusterRequest
-	(*ClusterResponse)(nil), // 1: provisioner.ClusterResponse
-}
+var file_provisioner_proto_goTypes = []interface{}{}
 var file_provisioner_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
@@ -151,45 +45,18 @@ func file_provisioner_proto_init() {
 	if File_provisioner_proto != nil {
 		return
 	}
-	if !protoimpl.UnsafeEnabled {
-		file_provisioner_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ClusterRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_provisioner_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ClusterResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_provisioner_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
 		GoTypes:           file_provisioner_proto_goTypes,
 		DependencyIndexes: file_provisioner_proto_depIdxs,
-		MessageInfos:      file_provisioner_proto_msgTypes,
 	}.Build()
 	File_provisioner_proto = out.File
 	file_provisioner_proto_rawDesc = nil
