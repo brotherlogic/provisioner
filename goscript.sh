a6429c2c251219541193cc7dd447636cabcca32a
Simon Tucker <brotherlogic@gmail.com>
2025-01-03 10:03:34 -0800

    Bump go version

--- goscript.sh
+++ goscript.sh
@@ -2,16 +2,16 @@ cd /home/simon
 rm -rf tmp/
 mkdir tmp
 cd tmp
-wget https://dl.google.com/go/go1.23.0.linux-armv6l.tar.gz
-tar xzf go1.23.0.linux-armv6l.tar.gz
+wget https://dl.google.com/go/go1.23.4.linux-armv6l.tar.gz
+tar xzf go1.23.4.linux-armv6l.tar.gz
 cd /usr/lib
-rm -rf go-1.23.0
-sudo mkdir go-1.23.0
-cd /usr/lib/go-1.23.0
+rm -rf go-1.23.4
+sudo mkdir go-1.23.4
+cd /usr/lib/go-1.23.4
 sudo rsync -az /home/simon/tmp/go/* ./
 cd /usr/lib
 sudo rm go
-sudo ln -s go-1.23.0 go
+sudo ln -s go-1.23.4 go
 cd /usr/bin
 sudo rm go
 sudo ln -s ../lib/go/bin/go ./go
