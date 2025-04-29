4b21863d898461359fd4f394323ee55913515c1e
Simon Tucker <brotherlogic@gmail.com>
2025-04-29 10:37:33 -0700

    Bumps go to 1.24.0

--- goscript64.sh
+++ goscript64.sh
@@ -2,16 +2,16 @@ cd /home/simon
 rm -rf tmp/
 mkdir tmp
 cd tmp
-wget https://dl.google.com/go/go1.23.4.linux-arm64.tar.gz
-tar xzf go1.23.4.linux-arm64.tar.gz
+wget https://dl.google.com/go/go1.24.0.linux-arm64.tar.gz
+tar xzf go1.24.0.linux-arm64.tar.gz
 cd /usr/lib
-rm -rf go-1.23.4
-sudo mkdir go-1.23.4
-cd /usr/lib/go-1.23.4
+rm -rf go-1.24.0
+sudo mkdir go-1.24.0
+cd /usr/lib/go-1.24.0
 sudo rsync -az /home/simon/tmp/go/* ./
 cd /usr/lib
 sudo rm go
-sudo ln -s go-1.23.4 go
+sudo ln -s go-1.24.0 go
 cd /usr/bin
 sudo rm go
 sudo ln -s ../lib/go/bin/go ./go
